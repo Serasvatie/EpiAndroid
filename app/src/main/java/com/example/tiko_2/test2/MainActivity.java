73537822e2091d3a29d683b9d49e3f5e5c60826b
tiko69 <nicolas.vieira.69360@gmail.com>
2015-01-20 18:06:23 +0100

    debut de page login

---
+++ app/src/main/java/com/example/tiko_2/test2/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.tiko_2.test2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.style.UpdateLayout;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+//import com.example.tiko_2.test2.HttpClient;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ProgressBar spinner;
+    private TextView Login;
+    private TextView Password;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = (ProgressBar) findViewById(R.id.progressBar1);
+        spinner.setVisibility(View.GONE);
+        Login = (TextView) findViewById(R.id.Login);
+        Password = (TextView) findViewById(R.id.Password);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void load(View view) {
+        spinner.setVisibility(View.VISIBLE);
+
+       HttpClient httpclient = new DefaultHttpClient();
+       HttpPost httppost = new HttpPost("http://epitech-api.herokuapp.com/login");
+
+       try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("login", "vieira_n"));
+            nameValuePairs.add(new BasicNameValuePair("password", "4urf2|uR"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+         //   HttpResponse response = httpclient.execute(httppost);  <-- c'est la ligne qui plante
+            Login.setText(response.getStatusLine().getStatusCode());
+        } catch (ClientProtocolException e) {
+           // TODO Auto-generated catch block
+        }
+       catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+
+    }
+
+}
