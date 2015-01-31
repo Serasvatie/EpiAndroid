ac2905e01bb46a4d9756947d57d72a25629224e8
favre_q <quentin.favre@epitech.eu>
2015-01-31 17:46:57 +0100

    Log time

--- app/src/main/java/com/epitech/EpiAndroid/Accueil.java
+++ app/src/main/java/com/epitech/EpiAndroid/Accueil.java
@@ -3,11 +3,12 @@ package com.epitech.EpiAndroid;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epitech.Model.GetBitMap;
 import com.epitech.Model.PostRequest;
@@ -23,6 +24,8 @@ import java.util.concurrent.ExecutionException;
 public class Accueil extends Fragment {
 
     ImageView photo;
+    TextView title;
+    TextView log;
     String login;
     String url;
     JSONObject infos;
@@ -50,14 +53,19 @@ public class Accueil extends Fragment {
         GetBitMap bitmap = new GetBitMap();
         try {
             photo = (ImageView) getView().findViewById(R.id.Photo);
+            title = (TextView) getView().findViewById(R.id.Title);
+            log = (TextView) getView().findViewById(R.id.Log);
             infos = req.execute("http://epitech-api.herokuapp.com/infos", "token", MainActivity.token.getToken()).get();
             login = infos.getJSONObject("infos").getString("login");
             req = new PostRequest();
             url = req.execute("http://epitech-api.herokuapp.com/photo", "token", MainActivity.token.getToken(), "login", login).get().getString("url");
             photo.setImageBitmap(bitmap.execute(url).get());
-            Log.d("URL", url);
+            title.setText(infos.getJSONObject("infos").getString("title"));
+            Double nbr = Double.parseDouble(infos.getJSONObject("current").getString("active_log"));
+            log.setText(getString(R.string.PatternLog) + String.valueOf(nbr.intValue()) + getString(R.string.Hour));
         } catch (ExecutionException | InterruptedException | JSONException | NullPointerException e) {
-             Log.d("ERROR", e.getMessage());
-       }
+            Toast toast = Toast.makeText(getActivity(), R.string.errorApi, Toast.LENGTH_LONG);
+            toast.show();
+         }
     }
 }
