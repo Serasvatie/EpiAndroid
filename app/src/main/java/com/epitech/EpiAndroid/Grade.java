82a36d2d003e8a36de565e8c4c585cee7bc1e9d7
favre_q <quentin.favre@epitech.eu>
2015-02-03 11:13:54 +0100

    Marks

--- app/src/main/java/com/epitech/EpiAndroid/Grade.java
+++ app/src/main/java/com/epitech/EpiAndroid/Grade.java
@@ -1,13 +1,16 @@
 package com.epitech.EpiAndroid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -27,7 +30,9 @@ public class Grade extends Fragment {
 
     JSONObject jsonreq;
     JSONArray tabModule;
+    JSONArray tabMarks;
     String arrayModule[];
+    String codeModule[];
 
     @Override
     public void onAttach(Activity activity) {
@@ -58,7 +63,12 @@ public class Grade extends Fragment {
             jsonreq = req.execute(getString(R.string.URLgetModules), getString(R.string.token), MainActivity.token.getToken()).get();
             tabModule = jsonreq.getJSONArray(getString(R.string.modules));
 
+            req = new PostRequest();
+            jsonreq = req.execute(getString(R.string.URLgetMarks), getString(R.string.token), MainActivity.token.getToken()).get();
+            tabMarks = jsonreq.getJSONArray(getString(R.string.notes));
+
             String tmparray[] = new String[tabModule.length()];
+            String tmpcodeModule[] = new String[tabModule.length()];
             int nbElem = 0;
             for (int i = 0; i < tabModule.length(); i++) {
                 try {
@@ -70,8 +80,10 @@ public class Grade extends Fragment {
                         tmparray[i] = tmparray[i] + getString(R.string.creditModule) + tmp.getString(getString(R.string.credits)) + "\n";
                         tmparray[i] = tmparray[i] + getString(R.string.moduleModule) + tmp.getString(getString(R.string.codemodule)) + "\n";
                         nbElem++;
+                        tmpcodeModule[i] = tmp.getString(getString(R.string.codemodule));
                     } else {
                         tmparray[i] = getString(R.string.Null);
+                        tmpcodeModule[i] = getString(R.string.Null);
                     }
                 } catch (JSONException e) {
                     Toast toast = Toast.makeText(getActivity(), R.string.errorParse, Toast.LENGTH_LONG);
@@ -80,12 +92,21 @@ public class Grade extends Fragment {
             }
 
             arrayModule = new String[nbElem];
+            codeModule = new String[nbElem];
             nbElem = 0;
             for (String tmp : tmparray)
             {
                 if (!tmp.equals(getString(R.string.Null)) && nbElem < arrayModule.length)
                 {
                     arrayModule[nbElem] = tmp;
+                    nbElem++;
+                }
+            }
+            nbElem = 0;
+            for (String tmp : tmpcodeModule) {
+                if (!tmp.equals(getString(R.string.Null)) && nbElem < codeModule.length)
+                {
+                    codeModule[nbElem] = tmp;
                     nbElem++;
                 }
             }
@@ -96,6 +117,53 @@ public class Grade extends Fragment {
             Toast toast = Toast.makeText(getActivity(), R.string.errorApi, Toast.LENGTH_LONG);
             toast.show();
         }
+        modules.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getString(R.string.marksProject));
+                String tmpBigArray[] = new String[tabMarks.length()];
+                int nbElem = 0;
+                for (int i = 0 ; i < tabMarks.length(); i++) {
+                    try {
+                        JSONObject tmpJSON = tabMarks.getJSONObject(i);
+                        if (tmpJSON.getString(getString(R.string.codemodule)).equals(codeModule[position])) {
+                            tmpBigArray[i] = tmpJSON.getString(getString(R.string.title)) + "\n";
+                            tmpBigArray[i] = tmpBigArray[i] + getString(R.string.notesDes) + tmpJSON.getString(getString(R.string.final_note)) + "\n";
+                            nbElem++;
+                        } else {
+                            tmpBigArray[i] = getString(R.string.Null);
+                        }
+                    } catch (JSONException e) {
+                        Toast toast = Toast.makeText(getActivity(), R.string.errorParse, Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+
+                String finalarray[] = new String[nbElem];
+                nbElem = 0;
+                for (String tmp : tmpBigArray) {
+                    if (!tmp.equals(getString(R.string.Null)) && nbElem < finalarray.length)
+                    {
+                        finalarray[nbElem] = tmp;
+                        nbElem++;
+                    }
+                }
+                ArrayAdapter<String> adpater = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, finalarray);
+                builder.setAdapter(adpater, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setPositiveButton(getString(R.string.OK), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
     private boolean isOnline() {
